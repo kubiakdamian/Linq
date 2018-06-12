318ecee9f005661308b0ec29fb0c2ab86a1d5859
Damian Kubiak <kubiak.damian96@gmail.com>
2018-06-12 20:34:54 +0200

    Reading xml file added

---
+++ Linq/DataReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Linq
+{
+    class DataReader
+    {
+        private String path;
+        private XElement xelement;
+        private IEnumerable<XElement> cds;
+        public DataReader()
+        {
+            this.path = "../../cds.xml";
+            xelement = XElement.Load(path);
+            cds = xelement.Elements();
+        }
+        
+        public void printData()
+        {
+            foreach (var cd in cds)
+            {
+                Console.WriteLine(cd);
+            }
+        }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Linq
+{
+    class Program
+    {
+        private static DataReader data;
+        static void Main(string[] args)
+        {
+            try
+            {
+                data = new DataReader();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Couldn't read data");
+                System.Threading.Thread.Sleep(5000);
+                Environment.Exit(0);
+            }
+            
+
+            data.printData();
+
+            Console.ReadLine();
+        }
+    }
+}
